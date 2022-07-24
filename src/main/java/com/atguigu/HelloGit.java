ae635fd019c23d7d5fae641d0217abb328e3f16d
qqinshaowu <1364941774@qq.com>
2022-07-24 15:21:46 +0800

    push commit

--- src/main/java/com/atguigu/HelloGit.java
+++ src/main/java/com/atguigu/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
         System.out.println("hello Git-3");
         System.out.println("Git Test 副本");
         System.out.println("Git Test1");
+        System.out.println("push Test");
     }
 }
